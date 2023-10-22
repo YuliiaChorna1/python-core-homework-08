cde1c9722fc58ed48736fcbe0382dfec2bd30d14
Yuliia Chorna <yuli.chorna@gmail.com>
2023-10-22 23:01:53 +1100

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-from datetime import date, datetime
+from datetime import date, timedelta, datetime
+from collections import defaultdict
 
+def get_period(start_date: date, days: int):
+    result = {}
+    for _ in range(days + 1):
+        result[start_date.day, start_date.month] = start_date.year
+        start_date += timedelta(days=1)
+    return result
 
-def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+def get_valid_workday(initial_date: date, period: dict) -> str:
+    workday = ""    
+
+    while initial_date.weekday() > 4:
+        initial_date += timedelta(days=1)
+
+    date_bd = initial_date.day, initial_date.month
+    if date_bd in list(period):
+        workday = initial_date.strftime("%A")
+    return workday
+    
+        
+def get_birthdays_per_week(users: list) -> list:
+    start_date = date.today()
+    period = get_period(start_date, 7)
+    
+    birthdays_per_week = defaultdict(list)
+
+    for user in users:
+        bd: date = user["birthday"]
+        date_bd = bd.day, bd.month
+        
+        if date_bd in list(period):
+            bd = bd.replace(year=period[date_bd])
+            workday = get_valid_workday(bd, period)
+            if workday:
+                birthdays_per_week[workday].append(user["name"])
+                            
+    return dict(birthdays_per_week)
 
 
 if __name__ == "__main__":
